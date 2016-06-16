eb8f5595934de3812217bf27ca89b951cfab777e
fyodr <tedmoriniv@gmail.com>
2016-06-16 16:48:49 -0400

    Updated the configuration runner to have it merge all sql additions into a single file

--- models/updateconfigs.py
+++ models/updateconfigs.py
@@ -2,20 +2,20 @@
 # updateconfigs.py
 # by Ted Morin
 #
-# updates all of the configuration files recursively
-# set criteria for "configs" at blue line
+# updates all of the configuration files recursively, merges the sql commands into a single file
+# set criteria for "configs" at blue lines
 
 
 import sys
-from os import *
-from os.path import isfile, isdir, join
+import os
 
 USAGE = 'USAGE: run_scripts_in [DEPTH] [DIRECTORY]'
 
-DEPTH = 1
-START = getcwd()
+DEPTH = 2
+CURDIR = os.getcwd()
+START = CURDIR
 
-
+# input checks
 if len(sys.argv) > 3:                           # too many arguments
     print USAGE
     print "ERROR: too many arguments"
@@ -28,35 +28,68 @@ elif 1 < len(sys.argv):                         # more than one argument
         print "ERROR: non-integer depth"
         exit(1)
     if len(sys.argv) == 3:
-        if isdir(sys.argv[2]):
+        if os.path.isdir(sys.argv[2]):
             START = sys.argv[2]
         else :                                  # invalid directory
             print USAGE
             print 'ERROR: directory invalid'
 
-# actual process here...
+# running scripts
 def run_scripts_in(mypath,recurs):
-    if recurs == DEPTH: return      #set depth here
+    if recurs == DEPTH: return   
     count = 0
     print mypath
-    chdir(mypath)
+    os.chdir(mypath)
     
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    notfiles = [f for f in listdir(mypath) if ( not isfile(join(mypath, f)) and f[0] != '.' )]
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+    notfiles = [f for f in os.listdir(mypath) if ( not os.path.isfile(os.path.join(mypath, f)) and f[0] != '.' )]
     onlyfiles.sort()
 ##############################################################################################################
     for f in onlyfiles:
-        if f[:5] == "new_c" and f[-3:] == '.py':
-            print join(mypath, f)
-            system('python "' + join(mypath, f) + '"')
+        if f[:5] == "new_c" and f[-3:] == '.py':        # change this line if lines are changed
+            print os.path.join(mypath, f)
+            os.system('python "' + os.path.join(mypath, f) + '"')
             count += 1
     
     for d in notfiles:
-         run_scripts_in(join(mypath,d),recurs+1)
+         run_scripts_in(os.path.join(mypath,d),recurs+1)
     print count
     print
 
+# combining sql commands
+def merge_sql_commands(mypath,recurs):
+    text = ""
+    if recurs == DEPTH: return text
+    
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+    notfiles = [f for f in os.listdir(mypath) if ( not os.path.isfile(os.path.join(mypath, f)) and f[0] != '.' )]
+    onlyfiles.sort()
+##############################################################################################################
+    for f in onlyfiles:
+        if f[:5] == "confi" and f[-4:] == '.sql':
+            infile = open(os.path.join(mypath, f),'r')
+            text += infile.read()
+            infile.close()
+            text += ';\n'
+    
+    for d in notfiles:
+        text += str(merge_sql_commands(os.path.join(mypath,d),recurs+1))
+    
+    return text
+    
+# run the scripts
 run_scripts_in(START,0)
 
+os.chdir(CURDIR)
+# get the commands
+text = merge_sql_commands(START,0)
+print "#############################"
+print text
+print "#############################"
 
 
+# save the merged sql commands in the starting directory
+os.chdir(CURDIR)
+output = open('merged.sql','w')
+output.write(text)
+output.close()
