53c86a63c5213f0f91e40cd3aec6b71fb5535a66
fyodr <tedmoriniv@gmail.com>
2016-06-16 08:06:32 -0400

    added model for 10.1161/01.CIR.96.1.44

---
+++ models/10.1161:01.CIR.96.1.44/model_b.py
@@ -0,0 +1,74 @@
+"""
+model_b.py
+by Ted Morin
+
+contains a function to predict 4-year risk of incident Intermittent Claudication using point system from
+10.1161/01.CIR.96.1.44
+1997 Intermittent Claudication: A Risk Profile From The Framingham Heart Study
+
+function expects parameters of
+'Sex' 'Age' 'Systolic BP' 'Diastolic BP' 'Cigarettes per day' 'Total Cholesterol' 'Diabetes' 'Previous CHD'
+      years     mm Hg          mm Hg           n/day                mg/dL  
+bool int/float int/float      int/float        int/float           int/float          bool        bool
+"""
+def model(ismale,age,sbp,dbp,cig,totchol,diab,chd):
+    
+    points = 0
+    
+    age_cutoffs = [50,55,60,65,70,75,80]
+    for cutoff in age_cutoffs:
+        if age >= cutoff:
+            points += 1
+    
+    if ismale :
+        points += 3
+    
+    if totchol < 170:
+        pass
+    elif totchol < 210:
+        points += 1
+    elif totchol < 250:
+        points += 2
+    elif totchol < 290:
+        points += 3
+    else : 
+        points += 4
+    
+    if (sbp < 130) and (dbp < 85):
+        points += 0
+    elif (sbp < 140) and (dbp < 90):
+        points += 1
+    elif (sbp < 160) and (dbp < 100):
+        points += 2
+    else :
+        points += 4
+    
+    if cig < 1:
+        pass
+    elif cig < 6:
+        points += 1
+    elif cig < 11:
+        points += 2
+    elif cig < 21:
+        points += 3
+    else :
+        points += 4
+    
+    if diab :
+        points += 5
+    if chd :
+        points += 5
+    
+    print points
+    
+    # look up risk
+    if points >= 10:
+        score = points - 10
+    else :
+        score = 0
+    
+    riskpercents = [1,1,1,2,2,2,3,3,4,5,6,7,8,10,11,13,16,18,21,24,28]
+    
+    risk = float(riskpercents[score]) / 100
+    
+    return risk
---
+++ models/10.1161:01.CIR.96.1.44/new_config_gener_b.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# a template for making config.json files for functions
+#import pprint
+#pp = pprint.PrettyPrinter(indent=4)
+
+config = {}
+
+# human and machine readable names for the model
+config['id'] = {}
+config['id']['DOI'] = '10.1161/01.CIR.96.1.44'                # DOI tag                       '99.9999/aaa.a9'
+config['id']['papertitle'] = 'Intermittent Claudication: A Risk Profile From The Framingham Heart Study' 
+config['id']['modeltitle'] = 'Point System'   
+config['id']['yearofpub'] = '1997'    
+config['id']['authors'] = ['Murabito, J.M.','DAgostino, R.B.']    
+
+# population constraints
+config['population'] = {}
+config['population']['must'] = ['']         # necessary attributes      ['Pedal Cyclist']
+config['population']['mustnot'] = ['Intermittent Claudication']
+config['population']['mustCUI'] = ['']      # CUIs for necessary        ['C0810085']
+config['population']['mustnotCUI'] = ['C0021775']   # CUIs for unacceptable     ['C1512018']
+
+# human and machine readable input descriptions
+config['input'] = {}
+config['input']['name'] = ['Sex','Age','Systolic BP','Diastolic BP','Cigarettes per day','Total Cholesterol','Diabetes','Previous CHD']
+config['input']['description'] = ['Male = True','Age','Systolic BP','Diastolic BP','Average Cigarettes per day','Total Cholesterol','Diabetes','Previous Coronary Heart Disease']
+config['input']['CUI'] = ['C28421','C0804405','C0488055','C0488052','C3169451','C0364708','C1315719','C2926063']
+config['input']['units'] = ['m=T','years','mmHg','mmHg','n/day','mg/dL','','']   
+config['input']['datatype'] = ['bool','float','float','float','float','float','bool','bool']
+config['input']['upper'] = ['','84','','','','','','']   
+config['input']['lower'] = ['','45','','','','','','']   
+
+# human and machine readable output descriptions
+config['output'] = {}
+config['output']['name'] = '4Y Risk of Intermittent Claudication'
+config['output']['outcomeName'] = 'Intermittent Claudication' 
+config['output']['outcomeTime'] = '4' 
+config['output']['CUI'] = 'C3176361'         
+config['output']['outcomeCUI'] = 'C0021775'  
+
+# tabular or machine readable data available for download
+config['data'] = {}
+config['data']['filename'] = ['']  
+config['data']['fileurl'] = ['']   
+config['data']['datumname'] = ['Sample Size'] 
+config['data']['datum'] = ['5209']     
+
+# model function and dependencies
+config['model'] = {}
+config['model']['language'] = 'python'      # function's language    'python'
+config['model']['uncompiled'] = ['model_b.py']  # some kind of pointer?  ['model.py']
+config['model']['compiled'] = ['']    # some kind of pointer?  ['']
+config['model']['dependList'] = 'requirements.txt'    # some kind of pointer?  'requirements.txt'
+config['model']['example'] = ['example_b.py']     # some kind of pointer?  ['example.py']
+
+# I do not know what this would be used for
+config['model_category'] = ['prognostic'] #choices: 'diagnostic','prognostic'
+
+# I do not know what these are for...
+config['predictive_ability'] = {}
+config['predictive_ability']['type'] = [] 
+config['predictive_ability']['metric'] = []
+config['predictive_ability']['value'] = []
+config['predictive_ability']['lcl'] = []
+config['predictive_ability']['ucl'] = []
+
+import json
+with open('config_b.json','w') as output:
+    json.dump(config,output)
