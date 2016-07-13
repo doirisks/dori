aad998ae3266066a20e6d818a2398395d298ec09
fyodr <tedmoriniv@gmail.com>
2016-07-13 08:43:19 -0400

    Added the current web interface and rough draft docker accomodations

--- setup/dockersetup.sh
+++ setup/dockersetup.sh
@@ -7,32 +7,36 @@
 #
 
 # update the container to make installation possible
-sudo apt-get update
+apt-get update
 
-# install git and pull to a known directory
-sudo apt-get install -y git
-mkdir /home/dori-master
-cd /home/dori-master
-git init
-git remote add origin https://github.com/doirisks/dori.git
-git pull origin master
+## install git and pull to a known directory
+#apt-get install -y git
+#mkdir /home/dori-master
+#cd /home/dori-master
+#git init
+#git remote add origin https://github.com/doirisks/dori.git
+#git pull origin master
 
 # install necessary python (condense to a requirements.txt or .yml or something?)
-sudo apt-get install -y python2.7
-sudo apt-get install -y python-sql
-sudo apt-get install -y python-MySQLdb
+apt-get install -y python2.7
+apt-get install -y python-sql
+apt-get install -y python-MySQLdb
+apt-get install -y python-numpy
+
+# install R?
+#TODO
 
 # start mysql server
-sudo service mysql start
+service mysql start
 
 # make a small sql command file, run it, and delete it
 # maybe do this step with python instead?
 echo "CREATE DATABASE doiarchive ; CREATE USER doirisks@localhost IDENTIFIED BY 'bitnami'; GRANT ALL PRIVILEGES ON doiarchive.* TO 'doirisks'@'localhost';" > sqlsetup.sql
 mysql -u root -p"Admin2015" < sqlsetup.sql
-rm sqlsetup.sql                                     
+rm sqlsetup.sql
 
 # run setup scripts individually
-cd setup
+cd /src/setup
 ./setup.sh
 
 # this should not be necessary TODO
@@ -40,3 +44,7 @@ sleep 1
 ./CUIs.py
 sleep 1
 ./CUIs.py
+
+# set interface as the DocumentRoot of the server
+sed "s/DocumentRoot .*/ServerRoot \/src\/interface\/public/g" /etc/apache2/sites-available/000-default.conf
+
