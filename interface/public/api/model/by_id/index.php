386ca6f7ec9268764616cb2bdf4fd6eda7208691
fyodr <tedmoriniv@gmail.com>
2016-07-14 14:01:55 -0400

    initial commit for model by id

--- interface/public/api/model/by_id/index.php
+++ interface/public/api/model/by_id/index.php
@@ -1,5 +1,34 @@
 <?php
+// get query configurations and posted json
 require('../../../../includes/query_conf.php'); 
-echo 'not implemented';
+$str_json = file_get_contents('php://input');
+$posted_array = json_decode($str_json, true);
 
+// make sure that request is valid
+if ( (!is_array($posted_array)) or (count($posted_array) == 0) ) {
+    var_dump($posted_array);
+    $ans['error'] = 'improper request';
+    exit();
+}
+
+// iterate through ids
+$ans = [];
+foreach( $posted_array as $id ) {
+    // ensure that id is valid
+    $id = (string)$id;
+    if ( !ctype_digit($id) ) {
+        $ans[$id]['error'] = 'invalid id';
+    }
+
+    // build, run, and store query
+    $to_query = "SELECT * FROM `models` WHERE id = " . $id ;
+    $ans[$id] = query($to_query);
+    
+    if ( empty($ans[$id]) ) {
+        $ans[$id]['error'] = 'no models found';
+    }
+}
+
+// send back the results
+echo json_encode($ans);
 ?>
