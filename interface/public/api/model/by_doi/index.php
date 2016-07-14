a7295f2e444944db47ac4f568901c4f519ade868
fyodr <tedmoriniv@gmail.com>
2016-07-14 13:46:00 -0400

    initial commit for model by doi

--- interface/public/api/model/by_doi/index.php
+++ interface/public/api/model/by_doi/index.php
@@ -1,5 +1,33 @@
 <?php
+// get query configurations and posted json
 require('../../../../includes/query_conf.php'); 
-echo 'not implemented';
+$str_json = file_get_contents('php://input');
+$posted_array = json_decode($str_json, true);
 
+// make sure that request is valid
+if ( !is_array($posted_array) or empty($posted_array) {
+    $ans['error'] = 'improper request';
+    
+    exit();
+}
+
+// iterate through DOIs
+$ans = [];
+foreach( $posted_array as $DOI ) {
+    // ensure that DOI is valid
+    if ( (strpos($DOI,"'") !== false) or (strpos($DOI,'"') !== false) ) {
+        $ans[$DOI]['error'] = 'invalid DOI';
+    }
+
+    // build, run, and store query
+    $to_query = "SELECT * FROM `models` WHERE DOI = '" . $DOI . "'";
+    $ans[$DOI] = query($to_query);
+    
+    if ( empty($ans[$DOI]) ) {
+        $ans[$DOI]['error'] = 'no models found'
+    }
+}
+
+// send back the results
+echo json_encode($ans);
 ?>
