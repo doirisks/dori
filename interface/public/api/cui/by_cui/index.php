fd1129dbd002df9cd1df6305ced093dd0e78e81c
fyodr <tedmoriniv@gmail.com>
2016-07-15 09:11:24 -0400

    initial commit for cui by cui in api

--- interface/public/api/cui/by_cui/index.php
+++ interface/public/api/cui/by_cui/index.php
@@ -1,5 +1,38 @@
 <?php
+// get query configurations and posted json
 require('../../../../includes/query_conf.php'); 
-echo 'not implemented';
+$str_json = file_get_contents('php://input');
+$posted_array = json_decode($str_json, true);
+
+// check that CUIs were sent (CUIs must be sent as an array!)
+if ( (!is_array($posted_array)) or (empty($posted_array)) ) {
+    $ans['error'] = 'no CUIs sent';
+    echo(json_encode($ans));
+    exit();
+}
+
+// iterate through CUIs
+foreach($posted_array as $CUI) {
+
+    // check for empty or dangerous CUIs
+    if ( ($CUI == NULL) or ($CUI == "") or ( !ctype_alnum($CUI) ) ){
+        $ans[$CUI]['error'] = 'bad CUI: ' . $CUI;
+        continue;
+    }
+    // send the query
+    $query_output = query( "SELECT * FROM `CUIs` WHERE CUI = '". htmlspecialchars ($CUI) ."'" )[0];
+    
+    // check that the response was valid
+    if ( empty($query_output) ) {
+        $ans[$CUI]['error'] = 'no CUI found: ' . $CUI;
+        continue;
+    }
+    
+    // store the data in the answer variable
+    $ans[$CUI] = $query_output;
+}
+
+// send back the data!
+echo json_encode($ans);
 
 ?>
