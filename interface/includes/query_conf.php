018036c935c452a276f865b6cf4a7957b6c217b9
fyodr <tedmoriniv@gmail.com>
2016-07-21 10:36:35 -0400

    made scoring process into a function

--- interface/includes/query_conf.php
+++ interface/includes/query_conf.php
@@ -138,4 +138,134 @@
             }
         }
     }
+    
+    
+    /**
+     * Scores an id based on [already expanded] CUI value table
+     */
+    function score_id($id, $CUIs, $CUI_vals) {
+        // response variable
+        $resp = [];
+    
+        // check that id is numerical
+        if ( !ctype_digit((string)$id) ){
+            $resp['error'] = 'bad id';
+            return($resp);
+        }
+
+        // get model data
+        $model = query("SELECT `DOI`, `compiled`, `uncompiled`, `language`, `inpCUI`, `inpdatatype`, `upper`, `lower`  FROM `models` WHERE `id` = " . $id )[0];
+
+        // unpack model data
+        $inputs = json_decode($model['inpCUI']);
+        $datatypes = json_decode($model['inpdatatype']);
+        $uppers = json_decode($model['upper']);
+        $lowers = json_decode($model['lower']);
+
+        // assemble and CHECK model arguments
+        $modelargs = array();
+        foreach ($inputs as $index => $CUI) {               // TODO check CUIs and arguments
+
+            // make sure a CUI value exists
+            if (!isset($CUI_vals[$CUI])) {
+                $resp['error'] = 'missing CUI: ' . $CUI;  // identify a missing CUI
+                return($resp);
+            }
+
+            // get the CUI's value
+            $arg = $CUI_vals[$CUI];
+            
+            // check that CUI's value is in valid range (unnecessary for bools!)
+            if (!empty($lowers[$index]) and ($arg < $lowers[$index])) {
+                $resp['error'] = 'CUI below acceptable range: ' . $CUI;  // CUI below acceptable range
+                return($resp);
+            }
+            if (!empty($uppers[$index]) and ($arg > $uppers[$index])) {
+                $resp['error'] = 'CUI above acceptable range: ' . $CUI;  // CUI above acceptable range
+                return($resp);
+            }
+            
+            if ($datatypes[$index] == 'bool') {     // bool, convert to integer
+                if ($arg === true) {
+                    $arg = '1';
+                } else if ($arg === false) {
+                    $arg = '0';
+                } else if ($arg == "true") {
+                    $arg = '1';
+                } else if ($arg == "false") {
+                    $arg = '0';
+                } else {
+                    $resp['error'] = 'bad boolean CUI: ' . $CUI;  // identify a bad bool
+                    return($resp);
+                }
+            } 
+            else if ($datatypes[$index] == 'integer' or $datatypes[$index] == 'int') { // integer
+                if ( ctype_digit($arg) ){
+                    // argument is already okay
+                } else if ( ctype_digit(str_replace('.','',$arg)) ) { // only non-numbers are decimal points
+                    if ( substr_count($arg,'.') == 1 && strlen($arg) > 1 ) { 
+                        // if there is only one decimal point, round to nearest integer
+                        $arg = (string)round((float)$arg);
+                    } else {
+                        // otherwise, error
+                        $resp['error'] = 'bad integer CUI: ' . $CUI;  // identify a bad CUI
+                        return($resp);
+                    }
+                } else {
+                    $resp['error'] = 'bad integer CUI: ' . $CUI;  // identify a bad CUI
+                    return($resp);
+                }
+            } 
+            else  {    // float ($datatypes[$index] == 'float')
+                if (ctype_digit($arg) ) {
+                    // if already integer, tell code it is a float
+                    $arg .= '.0';
+                } else if (ctype_digit(str_replace('.','',$arg)) ) {
+                    if (substr_count($arg,'.') == 1 && strlen($arg) > 1 ) {
+                        // arg is already fine
+                    }
+                    else {
+                        $resp['error'] = 'bad float CUI: ' . $CUI;  // identify a bad float
+                        return($resp);
+                    }
+                } else {
+                    $resp['error'] = 'bad float CUI: ' . $CUI;  // identify a bad float
+                    return($resp);
+                }
+            }
+            array_push($modelargs,$arg);
+        }
+        // make sure that no errors occurred
+        if (isset($resp['error'])) {
+            return($resp);
+        }
+
+        // calculate risk score
+        $command = null;
+        if ( strtolower($model['language']) == 'python' or strtolower($model['language']) == 'py') {
+            $command = 'python ../../../../scripts/pythonrisk.py "' . MODELROOT . '" "' . $model['DOI'] . '"';
+            $command .= " " . json_decode($model['uncompiled'])[0];
+            foreach( $modelargs as $arg) {
+                $command .= " " . $arg;
+            }
+        } else if ( strtolower($model['language']) == 'r' ) {
+            $command = 'Rscript ../../../../scripts/Rrisk.R "' . MODELROOT . '" "' . $model['DOI'] . '"';
+            $command .= " " . json_decode($model['compiled'])[0];
+            foreach( $modelargs as $arg) {
+                $command .= " " . $arg;
+            }
+        } else if ( strtolower($model['language']) == 'sas' ) {
+            //TODO
+        }
+
+        // calculate risk score
+        $modeloutput = array();
+        exec($command,$modeloutput);
+        if (count($modeloutput) < 1) {
+            $resp['error'] = "no response from model";
+        } else {
+            // record the score!
+            $resp['score'] = $modeloutput[0];
+        }
+    }
 ?>
