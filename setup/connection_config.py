fbd4a8a2c12313e6fe7ff68451c098bec16a5c86
fyodr <tedmoriniv@gmail.com>
2016-07-25 17:39:22 -0400

    apache and mysql containers are both running, but they are not yet linking effectively for purposes of the interface

--- setup/connection_config.py
+++ setup/connection_config.py
@@ -5,7 +5,7 @@ by Ted Morin
 SQL server connection configuration data for DORI setup files
 """
 
-DEFAULT_HOSTNAME = 'dorimysqldb'
+DEFAULT_HOSTNAME = 'dorimysqldb'  # try adding :3306 to the hostname!
 DEFAULT_DATEBASE = 'doiarchive'
 DEFAULT_USERNAME = 'doirisks'
 DEFAULT_PASSWORD = 'bitnami'
