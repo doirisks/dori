b6ded4c20b42144d447092aef3a049a959a88674
fyodr <tedmoriniv@gmail.com>
2016-08-02 15:43:08 -0400

    changed riskfactor table and model table to proper js objects

--- interface/public/js/models_table.js
+++ interface/public/js/models_table.js
@@ -4,31 +4,48 @@
  * class to write and maintain the models table (active models)
  **/
 
-function models_table(master) {
-    this.base = text;
+function model_list(master) {
+    // identify the master
     this.master = master;
-    var text = '<span id="modeltitle" style="width:400px"><span>';
-    text    += '  <span style="width:300px;float:left">Models</span>';
-    text    += '  <span style="width:75px">Scores</span>';
-    text    += '</span>';
-    text    += '<div id="allmodels" ></div>';
-    this.base = text;
     
+    this.base = document.createElement("div");
+    this.base.setAttribute("style","width:400px");
+    
+    //titlebar
+    var titletitle = document.createElement("p");
+    titletitle.setAttribute("style","width:325px;text-align:center;float:left;");
+    scoretitle.appendChild(docuemnt.createTextNode("Models");
+    var scoretitle = document.createElement("p");
+    scoretitle.setAttribute("style","width:325px;text-align:center;");
+    scoretitle.appendChild(docuemnt.createTextNode("Scores");
+    var titlebar = document.createElement("span");
+    titlebar.setAttribute("style","width:400px");
+    titlebar.appendChild(titletitle);
+    titlebar.appendChild(scoretitle);
+    
+    this.base.append(titlebar);
+    
+    // titles and scores lists
     this.titles = $("<div style='float:left; width:300px; display:block; overflow:hidden'></div>"); 
     this.scores = $("<div style='width:75px; display:block; overflow:hidden'></div>"); 
+    
+    this.base.append(this.titles);
+    this.base.append(this.scores);
     
     // the most recent addition
     this.head = null;
     
     // makes a CUI visible in the table
     this.push = function (model_obj) {
-        model_obj.show(this.head);
+        model_obj.show(this, this.head);
         this.head = model_obj;
     }
     
     // remove a CUI from the table
     this.pop = function (model_obj) {
+        if (this.head == model_obj) {
+            this.head = model_obj.prev;
+        }
         model_obj.remove();
-        //TODO
     }
 }
--- interface/public/js/riskfactors_table.js
+++ interface/public/js/riskfactors_table.js
@@ -4,32 +4,59 @@
  * class to write and maintain the risk factors table (risk factors in use)
  **/
 
-function riskfactors_table(master) {
+function riskfactor_list(master) {
     this.master = master;
-    var text = "            <form id = 'riskfactors' style='width:400px'>\n";
-    // TODO make cell formats into a css class
-    text    += '                <span id="riskfactortitles" style="width:300px"> <span style="width:20px"></span> <span style="width:180px">Risk Factor</span> <span style="width:50px"><p>Value</p></span> <span style="width:50px">Units</span> </span>';
-    text    += '                <div id="allriskfactors" class="wrapper"></div>\n';
-    text    += '                <!-- risk factor inputs added by getrisk.js -->\n';
-    text    += '            </form>\n';
-    this.base = text;
     
+    this.base = document.createElement("div");
+    this.base.setAttribute("style","width:400px");
+    
+    //titlebar
+    var removetitle = document.createElement("p");
+    titletitle.setAttribute("style","width:30px;text-align:center;float:left;");
+    scoretitle.appendChild(docuemnt.createTextNode("Models");
+    var nametitle = document.createElement("p");
+    scoretitle.setAttribute("style","width:150px;text-align:center;");
+    scoretitle.appendChild(docuemnt.createTextNode("Scores");
+    var inputtitle = document.createElement("p");
+    scoretitle.setAttribute("style","width:120px;text-align:center;");
+    scoretitle.appendChild(docuemnt.createTextNode("Scores");
+    var unitstitle = document.createElement("p");
+    scoretitle.setAttribute("style","width:50px;text-align:center;");
+    scoretitle.appendChild(docuemnt.createTextNode("Units");
+    var titlebar = document.createElement("span");
+    titlebar.setAttribute("style","width:400px");
+    titlebar.appendChild(removetitle);
+    titlebar.appendChild(namestitle);
+    titlebar.appendChild(inputtitle);
+    titlebar.appendChild(unitstitle);
+    
+    this.base.append(titlebar);
+    
+    // buttons, names, inputs, and units lists
     this.buttons = $("<div style='float:left; width:30px; display:block; overflow:hidden'></div>"); //document.createElement('span');
     this.names = $("<div style='float:left; width:150px; display:block; overflow:hidden'></div>"); //document.createElement('span');
     this.values = $("<div style='float:left; width:120px; display:block; overflow:hidden'></div>"); //document.createElement('span');
-    this.unit_names = $("<div style='style='width:50px; display:block; overflow:hidden'></div>"); //document.createElement('span');
+    this.unit_names = $("<div style='width:50px; display:block; overflow:hidden'></div>"); //document.createElement('span');
+    
+    this.base.append(this.buttons);
+    this.base.append(this.names);
+    this.base.append(this.values);
+    this.base.append(this.unit_names);
     
     // the most recent addition
     this.head = null;
     
     // makes a CUI visible in the table
     this.push = function (CUI_obj) {
-        CUI_obj.show(this.head);
+        CUI_obj.show(this, this.head);
         this.head = CUI_obj;
     }
     
     // remove a CUI from the table
     this.pop = function (CUI_obj) {
+        if (this.head == CUI_obj) {
+            this.head = CUI_obj.prev;
+        }
         CUI_obj.remove();
     }
 }
