2eec57d9bbe8c8ecb5377f0ec5e6c73befd54781
fyodr <tedmoriniv@gmail.com>
2016-08-02 15:23:15 -0400

    changed riskfactor finder and model finder into proper js objects

--- interface/public/js/models_finder.js
+++ interface/public/js/models_finder.js
@@ -3,17 +3,51 @@
  * 
  * class to hold the search for new models
  **/
-function models_finder(master) {
+function model_finder(master) {
+    // identify the master
     this.id = "newmodel";
     this.master = master;
-    var text = "        <form> <!-- add a new risk factor -->\n";
-    text    += '            <span class="searchform">\n';
-    text    += '                <span><b>Search for Models</b></span>\n';
-    text    += '                <div>\n';
-    text    += '                    <span><button id="'+this.id+'" onclick="the_interface.fetchmodels">Find</button></span>\n';
-    text    += '                    <span><input type="text" name="search"  style="text-align:center;width:100px"></input></span>\n';
-    text    += '                </div>\n';
-    text    += '            </span>\n';
-    text    += '        </form>\n';
-    this.base = text;
+    
+    // identify self for use in lambda functions
+    var _this = this;
+    
+    // function to search for and possibly add a model
+    this.find = function() {
+        // TODO
+    }
+    
+    // the base of the document
+    this.base = document.createElement('div');
+    var title = document.createElement('p');
+    var titletext = document.createElement('b');
+    titletext.appendChild(document.createTextNode("Search for Models"));
+    title.appendChild(titletext);
+    this.base.appendChild(title);
+    
+    // button
+    this.button = document.createElement('button');
+    this.button.setAttribute("style","float:left;");
+    this.button.appendChild(document.createTextNode("Find"));
+    $(this.button).click(function(e) {
+        e.preventDefault();
+        _this.find();
+    });
+    
+    // input
+    this.input = document.createElement("input");
+    this.input.setAttribute("type","text");
+    $(this.button).submit(function(e) {
+        e.preventDefault();
+        _this.find();
+    });
+    $(this.input).change(function () {    // autofill
+    
+        //TODO
+    });
+    
+    // the whole search form
+    var searchform = document.createElement('div');
+    searchform.appendChild(this.button);
+    searchform.appendChild(this.input);
+    this.base.appendChild(searchform);
 }
